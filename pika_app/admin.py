314e3ea1d02928d4957912bf15be678d3942cf9c
Evan Adkins <evan.adkins42@gmail.com>
2016-10-06 21:41:55 -0700

    add system to organize files: define recording_path function, replace filename with recording_file

--- pika_app/admin.py
+++ pika_app/admin.py
@@ -6,7 +6,9 @@ from .models import Observer, Collection, Recording
 class RecordingInline(admin.StackedInline):
     model = Recording
     extra = 1
-    fields = ["collection", "start_time", "filename", "device", "notes"]
+    fields = ["collection", "start_time", "recording_file",
+        ## EA 8/1/16 "filename",  replaced with "recording_file",
+        "device", "notes"]
 
 class CollectionAdmin(admin.ModelAdmin):
     inlines = [RecordingInline]
